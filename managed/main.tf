12c5e94c2f3aba4832453e88827d30b815aa5a39
Stefan Steinert <ststefa@heldenzeit.net>
2019-07-17 20:57:46 +0200

    doc

--- managed/main.tf
+++ managed/main.tf
@@ -1,3 +1,6 @@
+# credentials can be provided using shell variables
+#   export TF_VAR_username=john
+#   export TF_VAR_password=secret
 variable "username" {}
 variable "password" {}
 
--- unmanaged/main.tf
+++ unmanaged/main.tf
@@ -50,6 +50,9 @@ We can now refer to the networks by name (e.g. "splunk-net-az1-1"
 
 */
 
+# credentials can be provided using shell variables
+#   export TF_VAR_username=john
+#   export TF_VAR_password=secret
 variable "username" {}
 variable "password" {}
 
